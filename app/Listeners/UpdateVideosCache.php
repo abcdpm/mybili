e8dc9baeee7befa1443c074979080b57f9e31534
paccu <skydruids@gmail.com>
2026-03-26 17:35:40 +0800

    UpdateVideosCache使用ShouldBeUniqueUntilProcessing 防抖+保证最终一致性

--- app/Listeners/UpdateVideosCache.php
+++ app/Listeners/UpdateVideosCache.php
@@ -6,21 +6,22 @@ use App\Events\VideoPartUpdated;
 use App\Events\VideoUpdated;
 use App\Services\VideoManager\Contracts\VideoServiceInterface;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldBeUniqueUntilProcessing;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Cache;
 use Laravel\Horizon\Contracts\Silenced;
 
-class UpdateVideosCache implements ShouldQueue, ShouldBeUnique, Silenced
+class UpdateVideosCache implements ShouldQueue, ShouldBeUniqueUntilProcessing, Silenced
 {
     public $queue = 'fast';
 
     // 收到事件后，任务会等待 5 秒再执行。在这 5 秒的缓冲期内，
     // 刚好可以等待大批量的数据库写操作完成。
+    // 延迟 5 秒执行，合并这 5 秒内的所有更新
     public $delay = 5;
 
-    // 保证 10 秒内，Redis 队列里绝对不会出现第二个名为 `rebuild_videos_cache` 的任务。
-    public $uniqueFor = 10;
+    // 锁的兜底超时时间（可以设长一点，因为一旦开始执行锁就会被主动释放）
+    public $uniqueFor = 60;
 
     /**
      * Create the event listener.
@@ -29,6 +30,12 @@ class UpdateVideosCache implements ShouldQueue, ShouldBeUnique, Silenced
         protected VideoServiceInterface $videoService
     ) {}
 
+    // 指定唯一 ID
+    public function uniqueId(): string
+    {
+        return 'rebuild_videos_cache';
+    }
+
     /**
      * Handle the event.
      */
@@ -36,6 +43,7 @@ class UpdateVideosCache implements ShouldQueue, ShouldBeUnique, Silenced
     {
         // Laravel 底层会自动在 dispatch 时拦截重复事件，
         // 连队列都不会进，彻底告别刷屏和队列风暴。
+        // 直接全量更新缓存，底层会自动防抖且保证不漏尾刀
         $this->videoService->updateVideosCache();
     }
 }
