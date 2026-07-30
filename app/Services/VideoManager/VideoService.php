e4ab69e2e3cf3ac0ebd5ca351bb8760072040a0d
pharra <typechasing@gmail.com>
2026-07-30 20:55:14 +0000

    修复cover_image_url为空导致的exception (#97)
    * 修复cover_image_url为空导致的exception
    
    * Update VideoService.php

--- app/Services/VideoManager/VideoService.php
+++ app/Services/VideoManager/VideoService.php
@@ -375,7 +375,7 @@ class VideoService implements VideoServiceInterface
                 'audio_downloaded_num'  => $item['audio_downloaded_num'],
                 'frozen'                => $item['frozen'],
                 'invalid'               => $item['invalid'],
-                'cover_image_url'       => $item['cover_info']['image_url'],
+                'cover_image_url'       => data_get($item, 'cover_info.image_url') ?: null,
                 'created_at'            => $item['created_at'],
             ];
 
